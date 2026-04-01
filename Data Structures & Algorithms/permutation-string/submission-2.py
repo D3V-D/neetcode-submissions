a739565a05f04c8e41f1ee5a86f0a7a193041c44
D3V-D <98626679+D3V-D@users.noreply.github.com>
2026-04-01 11:12:50 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        targets = {}
+
+        for c in s1:
+            targets[c] = targets.get(c, 0) + 1
+        
+        # window of size len(s1)
+        l = 0
+        r = len(s1) - 1
+
+        # each char in window must be in set, otherwise cont.
+        while r < len(s2):
+            tt = targets.copy()
+            for i in range(l, r + 1):
+                if s2[i] in tt:
+                    tt[s2[i]] -= 1
+                    if tt[s2[i]] == 0:
+                        tt.pop(s2[i])
+                    if not tt:
+                        return True
+            r += 1
+            l += 1
+
+        return False
